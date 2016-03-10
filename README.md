c55c8bf47a8941b3c42475e90c5a0d743a93fd3c
Joshua Harms <joshua@nozzlegear.com>
2016-03-10 15:38:26 -0600

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# react-winjs.d.ts
-A Typescript/Typings definition for for ReactWinJS.
+# reqwest.d.ts
+
+An ambient Typescript/Typings definition for [https://github.com/winjs/react-winjs](ReactWinJS).
+
+### Installation
+
+Using [Typings](https://github.com/typings/typings):
+
+```bash
+typings install github:nozzlegear/reqwest.d.ts --save -A
+```
+
+Using NPM and [TSD](https://github.com/Definitelytyped/tsd):
+
+```bash
+npm install "git+https://git@github.com/nozzlegear/reqwest.d.ts.git"
+
+tsd link
+```
---
+++ react-winjs.d.ts
@@ -0,0 +1,174 @@
+declare module "react-winjs"
+{
+    import React from "react";
+    
+    interface WinJSComponent<T> extends React.Props<T>
+    {
+        className?: string;
+        
+        style?: React.CSSProperties;
+        
+        id?: string;
+        
+        "aria-controls"?: string;
+        
+        "aria-expanded"?: string;
+    }
+    
+    /**
+     * Creates a callback function that renders a piece of data with the
+     * @param data
+     */
+    function reactRenderer(data): (data) => (data) => React.Component<any, any> | JSX.Element;
+
+    class AppBar extends React.Component<any, any>{ }
+
+    namespace AppBar
+    {
+        class Button extends React.Component<any, any>{ }
+
+        class Toggle extends React.Component<{ selected: boolean; }, any>{ }
+
+        class Separator extends React.Component<any, any>{ }
+
+        /**
+        Provide the custom content as the child of this element.
+        */
+        class ContentCommand extends React.Component<any, any> { }
+
+        /**
+        Provide the flyout content as the child of this element.
+        */
+        class FlyoutCommand extends React.Component<any, any>{ }
+    }
+
+    class AutoSuggestBox extends React.Component<any, any>{ }
+
+    class BackButton extends React.Component<any, any> { }
+
+    class ContentDialog extends React.Component<any, any> { }
+
+    class DatePicker extends React.Component<any, any> { }
+
+    class FlipView extends React.Component<any, any> { }
+
+    /**
+    Provide the flyout content as the child of this element.
+    */
+    class Flyout extends React.Component<any, any> { }
+
+    class Hub extends React.Component<any, any> { }
+
+    namespace Hub
+    {
+        class Section extends React.Component<any, any>{ }
+    }
+
+    class ItemContainer extends React.Component<any, any>{ }
+
+    interface IListViewProps extends WinJSComponent<any>
+    {
+        /**
+         * The list view's header element.
+         */
+        headerComponent?: JSX.Element | JSX.ElementClass;
+
+        /**
+         * The list view's footer element. 
+         */
+        footerComponent?: JSX.Element | JSX.ElementClass;
+
+        /**
+         * The list view's layout type. Valid values are WinJS.UI.ListLayout or WinJS.UI.GridLayout.
+         */
+        layout: { type: any };
+
+        /**
+         * THe list view's observable data source. Use new WinJS.Binding.List([]).dataSource.
+         */
+        itemDataSource: WinJS.UI.IListDataSource<any>;
+
+        /**
+        A function that generates a React component. Use the returned function from ReactWinJS.reactRenderer().
+        */
+        itemTemplate: (data) => (data) => JSX.Element | JSX.ElementClass;
+        
+        /**
+         * Specifies how many ListView items can be selected.
+         */
+        selectionMode?: WinJS.UI.SelectionMode;
+        
+        itemsDraggable?: boolean;
+        
+        itemsReorderable?: boolean;
+        
+        /**
+         * Specifies how items in a ListView respond to the tap interaction.
+         */
+        tapBehavior?: WinJS.UI.TapBehavior;
+    }
+    
+    class ListView extends React.Component<IListViewProps, any> { }
+    
+    interface IMenuProps extends WinJSComponent<any>
+    {
+        button: any;
+        toggle: any;
+        separator: any;
+        flyoutCommand: React.ComponentClass<any>;
+    }
+
+    /**
+     * Every menu command must have a key 
+     */
+    class Menu extends React.Component<IMenuProps, any> { }
+
+    interface IPivotProps extends WinJSComponent<any>
+    {
+        customLeftHeaderComponent: React.Component<any, any>;    
+        customRightHeaderComponent: React.Component<any, any>;
+    }
+
+    /**
+     * Each Pivot.Item must have a key.
+     */
+    class Pivot extends React.Component<any, any> { }
+    
+    interface IRatingProps extends WinJSComponent<any>
+    {
+        maxRating: number;
+        userRating: number;
+    }
+    
+    class Rating extends React.Component<IRatingProps, any> { }
+    
+    class SemanticZoom extends React.Component<any, any> { }
+    
+    interface ISplitViewProps extends WinJSComponent<any>
+    {
+        paneComponent: React.ComponentClass<any>;
+        contentComponent: React.ComponentClass<any>;
+    }
+    
+    class SplitView extends React.Component<any, any> { }
+    
+    interface ISplitViewPaneToggleProps extends WinJSComponent<any>
+    {
+        paneOpened: boolean;
+    }
+    
+    class SplitViewPaneToggle extends React.Component<any, any> {}
+    
+    class TimePicker extends React.Component<any, any> {}
+    
+    class ToggleSwitch extends React.Component<any, any> {}
+    
+    class ToolBar extends React.Component<IMenuProps, any> {}
+    
+    interface ITooltipProps
+    {
+        contentComponent: React.ComponentClass<any>;
+    }
+    
+    class Tooltip extends React.Component<ITooltipProps, any> {}
+}
